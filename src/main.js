ab730550ed1ac15fba6210d5a35390a675327d10
Yingting Huang <yhuan300@ucsc.edu>
2024-01-28 16:09:21 -0800

    first

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config =
+{
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    render: {
+        pixelArt: true
+    },
+    scene: [ Menu, Play ]
+}
+let game = new Phaser.Game(config)
+let borderUIsize = game.config.height / 15
+let borderPadding = borderUIsize / 3
+
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,49 @@
+class Spaceship extends Phaser.GameObjects.Sprite
+{
+    constructor(scene, x, y, texture, frame, pointValue)
+    {
+        super(scene, x, y, texture, frame)
+        scene.add.existing(this)
+        this.points = pointValue
+        this.moveSpeed = game.settings.spaceshipSpeed
+    }
+
+    update()
+    {
+        this.x -= this.moveSpeed
+        if(this.x <= 0 - this.width)
+        {
+            this.reset()
+        }
+    }
+
+    reset()
+    {
+        this.x = game.config.width
+    }
+}
+
+class Pink extends Phaser.GameObjects.Sprite
+{
+    constructor(scene, x, y, texture, frame, pointValue)
+    {
+        super(scene, x, y, texture, frame)
+        scene.add.existing(this)
+        this.points = pointValue 
+        this.moveSpeed = game.settings.pinkSpeed
+    }
+
+    update()
+    {
+        this.x -= this.moveSpeed
+        if(this.x <= 0 - this.width)
+        {
+            this.reset()
+        }
+    }
+
+    reset()
+    {
+        this.x = game.config.width
+    }
+}
